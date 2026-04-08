fcb65201662c7e25ae2ee16a3e3a185d15920f5a
Dietmar Wallmann <199122292+DimschAusD@users.noreply.github.com>
2026-04-08 18:18:59 +0200

    Add FullPath-to-Name converter and use in UI
    Introduce ConverterFullPathToNameConverter (Converters/ConverterFullPathToNameConverter.cs) which trims trailing separators and returns Path.GetFileName for a given full path (ConvertBack not implemented). Register the converter in MainWindow.xaml and apply it to the SelectedBildchen.BName binding so the UI displays the filename instead of the full path.

---
+++ Converters/ConverterFullPathToNameConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+
+namespace TestImage.Converters
+{
+    internal class ConverterFullPathToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+
+            if (value is not string fullPath || string.IsNullOrWhiteSpace(fullPath))
+                return value;
+
+            // Entfernt evtl. abschließende \ oder /
+            fullPath = fullPath.TrimEnd(
+                Path.DirectorySeparatorChar,
+                Path.AltDirectorySeparatorChar);
+
+            return Path.GetFileName(fullPath);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
