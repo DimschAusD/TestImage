8b97fba4caee495813cf953a1a99046ef36992ac
Dietmar Wallmann <199122292+DimschAusD@users.noreply.github.com>
2026-06-27 14:45:38 +0200

    Add VerschobenSegment converter and segmented UI indicator
    Introduce ConverterVerschobenSegment (IMultiValueConverter) to color quadrant segments gold when verschoben/gesamt reaches segment thresholds. Register the converter as a resource in MainWindow.xaml and add a 4-segment Border indicator wired with MultiBindings to CountBildchenFürLinks and AufgabenView.Count plus a position TextBlock bound to AufgabenView.CurrentPosition. Also include minor XAML formatting/whitespace cleanups.

---
+++ Converters/ConverterVerschobenSegment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TestImage.Converters
+{
+    internal class ConverterVerschobenSegment : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (values.Length < 2)
+            {
+                return Brushes.LightGray;
+            }
+
+            int verschoben = System.Convert.ToInt32(values[0]);
+            int gesamt = System.Convert.ToInt32(values[1]);
+
+            if (gesamt == 0)
+            {
+                return Brushes.LightGray;
+            }
+
+            double quote = verschoben / (double)gesamt;
+
+            int segment = int.Parse(parameter!.ToString()!);
+
+            double grenze = segment / 4.0;
+
+            return quote >= grenze
+                ? Brushes.Gold
+                : Brushes.LightGray;
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
